c53b3a44cd9931cca2a0522a9db6a07cbb815a5e
ptduy14 <phantanduy14@gmail.com>
2024-08-31 15:21:33 +0700

    feat: add navigation movie list

--- components/commons/movie-list.tsx
+++ components/commons/movie-list.tsx
@@ -4,6 +4,9 @@ import NewlyMovieItem from './newly-movie-item';
 import NewlyMovie from 'types/newly-movie';
 import Movie from 'types/movie';
 import RegularMovieItem from './regular-movie-item';
+import { GrNext } from 'react-icons/gr';
+import { GrPrevious } from 'react-icons/gr';
+import { useRef } from 'react';
 
 interface MovieListProps {
   listName: string;
@@ -12,10 +15,20 @@ interface MovieListProps {
 }
 
 export default function MovieList({ listName, movies, isNewlyMovieItem }: MovieListProps) {
+  const swiperRef = useRef<any>(null);
+
+  const handlePrevSlide = () => {
+    swiperRef.current.slidePrev();
+  }
+
+  const handleNextSlide = () => {
+    swiperRef.current.slideNext();
+  }
+  
   return (
     <div className="container-wrapper space-y-4">
       <h2 className="text-2xl font-semibold">{listName}</h2>
-      <Swiper slidesPerView={5} spaceBetween={30} loop={true} className="mySwiper">
+      <Swiper slidesPerView={5} spaceBetween={30} loop={true} onSwiper={(swiper) => swiperRef.current = swiper} className="mySwiper relative group">
         {isNewlyMovieItem
           ? movies.map((movie) => (
               <SwiperSlide key={movie._id}>
@@ -27,6 +40,12 @@ export default function MovieList({ listName, movies, isNewlyMovieItem }: MovieL
                 <RegularMovieItem movie={movie as Movie} />
               </SwiperSlide>
             ))}
+        <div className="absolute hidden group-hover:flex items-center top-0 left-0 bottom-0 w-[15%] bg-gradient-to-r from-black z-10">
+          <GrPrevious size={30} className='cursor-pointer' onClick={handlePrevSlide}/>
+        </div>
+        <div className="absolute hidden group-hover:flex items-center justify-end top-0 right-0 bottom-0 w-[15%] bg-gradient-to-l from-black z-10">
+          <GrNext size={30} className='cursor-pointer' onClick={handleNextSlide}/>
+        </div>
       </Swiper>
     </div>
   );
