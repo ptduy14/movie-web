8fac00b1177c73dd7d999507767bfe091c9c6db1
ptduy14 <phantanduy14@gmail.com>
2024-09-01 14:52:58 +0700

    refactor: update navigation logo click

--- components/header/header-default.tsx
+++ components/header/header-default.tsx
@@ -6,9 +6,9 @@ export default function HeaderDefault() {
     return (
         <header className="py-4 absolute top-0 left-0 right-0 z-10">
           <div className="header-container flex items-center justify-between container-wrapper">
-            <div>
+            <a className="block" href="/">
               <Image src={logo} alt="Picture of the author" className="w-32" />
-            </div>
+            </a>
             <ul className="flex items-center font-semibold">
               <li className="px-9">Phim lẻ</li>
               <li className="px-9">Phim bộ</li>
