1ccefd92071b75fc0af96a2b194504e313b4bacf
ptduy14 <phantanduy14@gmail.com>
2024-09-10 12:59:23 +0700

    feat: complete change episode

--- components/watch/index.tsx
+++ components/watch/index.tsx
@@ -1,12 +1,44 @@
 'use client';
 import DetailMovie from 'types/detail-movie';
 import VideoPlayer from './video-player';
+import { useState } from 'react';
 
 export default function MovieWatchPage({ movie }: { movie: DetailMovie }) {
-  console.log(movie.episodes[0].server_data[0].link_embed);
+  const [episodeIndex, setEpisodeIndex] = useState<number>(0);
+  const [episodeLink, setEpisodeLink] = useState<string>(
+    movie.episodes[0].server_data[0].link_m3u8
+  );
+
+  const handleSwitchEpisode = (index: number) => {
+    setEpisodeIndex(index);
+    setEpisodeLink(movie.episodes[0].server_data[index].link_m3u8);
+  };
+
   return (
-    <div className="pt-[3.75rem]">
-        <VideoPlayer videoUrl={movie.episodes[0].server_data[0].link_m3u8} thumbnail={movie.movie.thumb_url}/>
+    <div className="pt-[3.75rem] space-y-10">
+      <VideoPlayer videoUrl={episodeLink} thumbnail={movie.movie.thumb_url} />
+      <div className="container-wrapper-movie">
+        <h1 className="text-3xl">{movie.movie.name}</h1>
+        <h3 className="text-lg text-[#bbb6ae] mt-2">{movie.movie.origin_name}</h3>
+      </div>
+      {movie.movie.type === 'series' && (
+        <div className="container-wrapper-movie">
+          <h1 className="text-xl">Danh sách tập phim</h1>
+          <ul className="flex flex-wrap gap-3 mt-4">
+            {movie.episodes[0].server_data.map((ep, index) => (
+              <li
+                key={index}
+                className={`block ${
+                  episodeIndex === index ? 'text-white bg-[#5E5E5E]' : 'bg-white text-black hover:bg-[#d3d3d3]'
+                } px-3 py-2 rounded-md font-semibold cursor-pointer`}
+                onClick={() => handleSwitchEpisode(index)}
+              >
+                {ep.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
--- components/watch/video-player.tsx
+++ components/watch/video-player.tsx
@@ -1,10 +1,10 @@
 import React, { useRef, useEffect } from 'react';
 import Hls from 'hls.js';
-import { FaPlay } from "react-icons/fa";
+import { FaPlay } from 'react-icons/fa';
 
 const VideoPlayer = ({ videoUrl, thumbnail }: { videoUrl: string; thumbnail: string }) => {
   const videoRef = useRef<HTMLVideoElement | null>(null);
-  const overlay = useRef<HTMLDivElement | null>(null)
+  const overlay = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     const video = videoRef.current;
@@ -24,21 +24,22 @@ const VideoPlayer = ({ videoUrl, thumbnail }: { videoUrl: string; thumbnail: str
         video.removeAttribute('src'); // Stop the video stream
         video.load();
       }
+      overlay.current?.classList.remove('hidden');
     };
   }, [videoUrl]);
 
   const handlePlayVideo = () => {
     videoRef.current?.play();
     overlay.current?.classList.add('hidden');
-  }
+  };
 
   return (
     <div className="relative w-full h-[34rem]">
       <video ref={videoRef} controls style={{ width: '100%', height: '100%' }} />
       <div ref={overlay} className="absolute inset-0 bg-black flex items-center justify-center">
         <img src={thumbnail} alt="" className="h-full object-center object-cover" />
-        <FaPlay className='absolute cursor-pointer z-10' size={40} onClick={handlePlayVideo}/>
-        <div className='bg-red-600 absolute inset-0 opacity-5'></div>
+        <FaPlay className="absolute cursor-pointer z-10 hover:scale-125 transition-all duration-200" size={40} onClick={handlePlayVideo} />
+        <div className="bg-red-600 absolute inset-0 opacity-5"></div>
       </div>
     </div>
   );
