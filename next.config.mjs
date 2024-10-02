2cba7570b90ede7e77d95d362626dd54537670c3
ptduy14 <phantanduy14@gmail.com>
2024-10-02 17:09:30 +0700

    style: fix img movie is cover

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    domains: ['img.ophim.live'], // Thêm domain img.ophim.live
+  },
 };
 
 export default nextConfig;
