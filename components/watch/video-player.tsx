dc6788cd66ccd156c423102fdf807df2ef9d0b80
ptduy14 <ptduy14@gmail.com>
2024-10-07 23:16:00 +0700

    refactor: resolve potential problems

--- components/watch/video-player.tsx
+++ components/watch/video-player.tsx
@@ -8,58 +8,72 @@ type VideoPlayerProps = {
   videoProgress: number | null;
 };
 
-const VideoPlayer = ({ videoUrl, thumbnail, videoProgress }: VideoPlayerProps, videoRef: React.Ref<HTMLVideoElement> | null) => {
-  const overlay = useRef<HTMLDivElement | null>(null);
+const VideoPlayer = forwardRef<HTMLVideoElement, VideoPlayerProps>(
+  ({ videoUrl, thumbnail, videoProgress }, videoRef) => {
+    const overlay = useRef<HTMLDivElement | null>(null);
 
-  useEffect(() => {
-    const video = videoRef && 'current' in videoRef ? videoRef.current : null;
-    if (video) {
-      if (Hls.isSupported()) {
-        const hls = new Hls();
-        hls.loadSource(videoUrl);
-        hls.attachMedia(video);
-      } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-        // For Safari and other browsers that support HLS natively
-        video.src = videoUrl;
+    useEffect(() => {
+      if (!videoUrl) return; 
+
+      const video = videoRef && 'current' in videoRef ? videoRef.current : null;
+      
+      if (video) {
+        if (Hls.isSupported()) {
+          const hls = new Hls();
+          hls.loadSource(videoUrl);
+          hls.attachMedia(video);
+        } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+          // For Safari and other browsers that support HLS natively
+          video.src = videoUrl;
+        }
+        
+        if (videoProgress) {
+          video.currentTime = videoProgress
+          video.addEventListener('canplaythrough', () => handleAutoPlay(video))
+        };
       }
-      if (videoProgress) {
-        video.currentTime = videoProgress
-        video.addEventListener('canplaythrough', (e) => {
-          video.play();
-          overlay.current?.classList.add('hidden');
-        })
+
+      console.log(videoUrl)
+
+      return () => {
+        if (video && video.src) {
+          video.pause();
+          video.removeAttribute('src'); // Stop the video stream
+          video.load();
+        }
+
+        if (videoProgress && video && video.src) {
+          video.removeEventListener('canplaythrough', () => handleAutoPlay(video));
+        }
+
+        overlay.current?.classList.remove('hidden');
       };
+    }, [videoUrl, videoProgress]);
+
+    const handleAutoPlay = (video: any) => {
+        video.play();
+        overlay.current?.classList.add('hidden');
     }
 
-    return () => {
-      if (video && video.src) {
-        video.pause();
-        video.removeAttribute('src'); // Stop the video stream
-        video.load();
-      }
-      overlay.current?.classList.remove('hidden');
+    const handlePlayVideo = () => {
+      if (videoRef && 'current' in videoRef) videoRef.current?.play();
+      overlay.current?.classList.add('hidden');
     };
-  }, [videoUrl, videoProgress]);
 
-  const handlePlayVideo = () => {
-    if (videoRef && 'current' in videoRef) videoRef.current?.play();
-    overlay.current?.classList.add('hidden');
-  };
-
-  return (
-    <div className="relative w-full h-[34rem]">
-      <video ref={videoRef} controls style={{ width: '100%', height: '100%' }} />
-      <div ref={overlay} className="absolute inset-0 bg-black flex items-center justify-center">
-        <img src={thumbnail} alt="" className="h-full object-center object-cover" />
-        <FaPlay
-          className="absolute cursor-pointer z-10 hover:scale-125 transition-all duration-200"
-          size={40}
-          onClick={handlePlayVideo}
-        />
-        <div className="bg-red-600 absolute inset-0 opacity-5"></div>
+    return (
+      <div className="relative w-full h-[34rem]">
+        <video ref={videoRef} controls style={{ width: '100%', height: '100%' }} />
+        <div ref={overlay} className="absolute inset-0 bg-black flex items-center justify-center">
+          <img src={thumbnail} alt="" className="h-full object-center object-cover" />
+          <FaPlay
+            className="absolute cursor-pointer z-10 hover:scale-125 transition-all duration-200"
+            size={40}
+            onClick={handlePlayVideo}
+          />
+          <div className="bg-red-600 absolute inset-0 opacity-5"></div>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  });
 
-export default forwardRef(VideoPlayer) ;
+export default VideoPlayer;
