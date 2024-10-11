97bab01307453d9e0ea73b2184accaa535b94e5c
ptduy14 <ptduy14@gmail.com>
2024-10-11 19:39:37 +0700

    refactor: refactor logic autoplay when chooosing contiue progress watch and adding loading init movie

---
+++ components/loading/loading-spiner-video-player.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function LoadingSpinnerVideoPlayer() {
+  return (
+    <svg
+      className="absolute z-10 animate-spin cursor-pointer hover:scale-125 transition-all duration-200"
+      width={40}
+      height={40}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle
+        className="opacity-25"
+        cx="12"
+        cy="12"
+        r="10"
+        stroke="currentColor"
+        strokeWidth="4"
+      ></circle>
+      <path
+        className="opacity-75"
+        fill="currentColor"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
+      ></path>
+    </svg>
+  );
+};
--- components/watch/video-player.tsx
+++ components/watch/video-player.tsx
@@ -1,6 +1,7 @@
-import React, { useRef, useEffect, forwardRef } from 'react';
+import React, { useRef, useEffect, forwardRef, useState } from 'react';
 import Hls from 'hls.js';
 import { FaPlay } from 'react-icons/fa';
+import LoadingSpinnerVideoPlayer from '../loading/loading-spiner-video-player';
 
 type VideoPlayerProps = {
   videoUrl: string;
@@ -11,12 +12,13 @@ type VideoPlayerProps = {
 const VideoPlayer = forwardRef<HTMLVideoElement, VideoPlayerProps>(
   ({ videoUrl, thumbnail, videoProgress }, videoRef) => {
     const overlay = useRef<HTMLDivElement | null>(null);
+    const [isCanPlay, setIsCanPlay] = useState<boolean>(false);
 
     useEffect(() => {
-      if (!videoUrl) return; 
+      if (!videoUrl) return;
 
       const video = videoRef && 'current' in videoRef ? videoRef.current : null;
-      
+
       if (video) {
         if (Hls.isSupported()) {
           const hls = new Hls();
@@ -26,14 +28,13 @@ const VideoPlayer = forwardRef<HTMLVideoElement, VideoPlayerProps>(
           // For Safari and other browsers that support HLS natively
           video.src = videoUrl;
         }
-        
+
         if (videoProgress) {
-          video.currentTime = videoProgress
-          video.addEventListener('canplaythrough', () => handleAutoPlay(video))
-        };
+          video.currentTime = videoProgress;
+        }
       }
 
-      console.log(videoUrl)
+      console.log(videoUrl);
 
       return () => {
         if (video && video.src) {
@@ -42,39 +43,59 @@ const VideoPlayer = forwardRef<HTMLVideoElement, VideoPlayerProps>(
           video.load();
         }
 
-        if (videoProgress && video && video.src) {
-          video.removeEventListener('canplaythrough', () => handleAutoPlay(video));
-        }
-
         overlay.current?.classList.remove('hidden');
       };
     }, [videoUrl, videoProgress]);
 
-    const handleAutoPlay = (video: any) => {
+    const handleCanPlayThrough = (video: any) => {
+      setIsCanPlay(true);
+
+      if (videoProgress) {
         video.play();
         overlay.current?.classList.add('hidden');
-    }
+      }
+    };
 
     const handlePlayVideo = () => {
       if (videoRef && 'current' in videoRef) videoRef.current?.play();
       overlay.current?.classList.add('hidden');
     };
 
+    useEffect(() => {
+      if (videoRef && 'current' in videoRef) {
+        videoRef.current?.addEventListener('canplaythrough', () =>
+          handleCanPlayThrough(videoRef.current)
+        );
+      }
+
+      return () => {
+        if (videoRef && 'current' in videoRef)
+          videoRef.current?.addEventListener('canplaythrough', () =>
+            handleCanPlayThrough(videoRef.current)
+          );
+      };
+    }, [videoProgress]);
+
     return (
       <div className="relative w-full h-[34rem]">
         <video ref={videoRef} controls style={{ width: '100%', height: '100%' }} />
         <div ref={overlay} className="absolute inset-0 bg-black flex items-center justify-center">
           <img src={thumbnail} alt="" className="h-full object-center object-cover" />
-          <FaPlay
-            className="absolute cursor-pointer z-10 hover:scale-125 transition-all duration-200"
-            size={40}
-            onClick={handlePlayVideo}
-          />
+          {isCanPlay ? (
+            <FaPlay
+              className="absolute cursor-pointer z-10 hover:scale-125 transition-all duration-200"
+              size={40}
+              onClick={handlePlayVideo}
+            />
+          ) : (
+            <LoadingSpinnerVideoPlayer />
+          )}
           <div className="bg-red-600 absolute inset-0 opacity-5"></div>
         </div>
       </div>
     );
-  });
+  }
+);
 
 // Set a display name for the component
 VideoPlayer.displayName = 'VideoPlayer';
