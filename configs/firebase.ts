32bd14d56f5a28553f73268f740f5e73cc1de3c2
ptduy14 <ptduy14@gmail.com>
2024-10-12 09:46:34 +0700

    config firebase

---
+++ configs/firebase.ts
@@ -0,0 +1,22 @@
+import { getAuth } from "firebase/auth";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "moviex-ad32a.firebaseapp.com",
+  projectId: "moviex-ad32a",
+  storageBucket: "moviex-ad32a.appspot.com",
+  messagingSenderId: "621172853293",
+  appId: "1:621172853293:web:b848576bfb885700b446a4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
+export const auth = getAuth();
