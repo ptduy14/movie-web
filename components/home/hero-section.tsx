d837efe3b0a9aeca7405738a35c57367a8407a0e
ptduy14 <ptduy14@gmail.com>
2024-10-15 15:24:52 +0700

    style: add btn to next slide hero section item

--- components/home/hero-section.tsx
+++ components/home/hero-section.tsx
@@ -9,13 +9,13 @@ import { getDetailMovieServerAction } from 'app/actions';
 import DetailMovie from 'types/detail-movie';
 import LoadingComponent from '../loading/loading-component';
 import { useHomePageLoadingContext } from '../context/home-page-loading-context';
+import { FaChevronRight } from 'react-icons/fa6';
 
 export default function HeroSection({ movies }: { movies: NewlyMovie[] }) {
   const [detailMovies, setDetailMovies] = useState<DetailMovie[]>([]);
   const swiperRef = useRef<any>(null);
-  const [isFetchingDetailMovie, setIsFetchingDetailMovie] = useState<boolean>(false);
 
-  const {isLoadingHomePage, setISLoadingHomePage} = useHomePageLoadingContext();
+  const { isLoadingHomePage, setISLoadingHomePage } = useHomePageLoadingContext();
 
   useEffect(() => {
     const getDescriptionMovies = async () => {
@@ -28,28 +28,33 @@ export default function HeroSection({ movies }: { movies: NewlyMovie[] }) {
   }, [movies]);
 
   const handleClickToNextSlide = () => {
-    // swiperRef.current?.slideNext();
+    swiperRef.current?.slideNext();
   };
 
   if (isLoadingHomePage) {
-    return <LoadingComponent />
+    return <LoadingComponent />;
   }
 
   return (
-    <Swiper
-      modules={[EffectFade, Autoplay]}
-      effect="fade"
-      autoplay={{ delay: 4000 }}
-      loop={true}
-      onSwiper={(swiper) => (swiperRef.current = swiper)}
-    >
-      {detailMovies.map((movie: DetailMovie) => {
-        return <SwiperSlide key={movie.movie._id} onClick={handleClickToNextSlide}>
-        <HeroMovieItem
-          detailMovie={movie}
-        />
-      </SwiperSlide>
-      })}
-    </Swiper>
+    <div className='relative'>
+      <Swiper
+        modules={[EffectFade, Autoplay]}
+        effect="fade"
+        autoplay={{ delay: 10000 }}
+        loop={true}
+        onSwiper={(swiper) => (swiperRef.current = swiper)}
+      >
+        {detailMovies.map((movie: DetailMovie) => {
+          return (
+            <SwiperSlide key={movie.movie._id}>
+              <HeroMovieItem detailMovie={movie}/>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+      <div className="absolute z-10 top-[20rem] right-6 border border-white p-4 rounded-full group hover:border-black hover:bg-white cursor-pointer transition-all duration-300" onClick={handleClickToNextSlide}>
+        <FaChevronRight className="text-white group-hover:text-black transition-colors duration-300" />
+      </div>
+    </div>
   );
 }
