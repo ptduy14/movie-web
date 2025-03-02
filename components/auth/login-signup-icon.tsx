5d02bd499984aac9c5bff0b684444272448fc10f
ptduy14 <phantanduy14@gmail.com>
2025-03-02 15:57:04 +0700

    refactor: try catch method of firebase-service

--- components/auth/login-signup-icon.tsx
+++ components/auth/login-signup-icon.tsx
@@ -1,12 +1,15 @@
 import { CgProfile } from 'react-icons/cg';
 import { useAuthModel } from '../context/auth-modal-context';
 
-export default function LoginSignUpIcon({ isShowFixedHeader }: { isShowFixedHeader: boolean }) {
-  const {openAuthModal} = useAuthModel();
+export default function LoginSignUpIcon() {
+  const { openAuthModal } = useAuthModel();
   return (
     <>
-        <CgProfile className={`cursor-pointer hover:text-custome-red]`} size={25} onClick={() => openAuthModal()}/>
-        
+      <CgProfile
+        className={`cursor-pointer hover:text-custome-red]`}
+        size={25}
+        onClick={() => openAuthModal()}
+      />
     </>
   );
 }
