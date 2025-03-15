20ee1482f609ef0488359f0823b8d2f6a7360dcd
ptduy14 <phantanduy14@gmail.com>
2025-03-15 21:14:07 +0700

    refactor: authenticating firebase admin using env

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,11 +1,13 @@
-import admin from "firebase-admin";
+import admin from 'firebase-admin';
 import { getFirestore } from 'firebase-admin/firestore';
-import { getApps, initializeApp } from "firebase-admin/app";
+import { getApps, initializeApp } from 'firebase-admin/app';
 
-var serviceAccount = require("../firebase-admin.json");
+var serviceAccount = process.env.MY_FIREBASE_CREDENTIALS!;
 
 // Chỉ khởi tạo Firebase admin nếu chưa có khởi tạo
-const appAdmin = !getApps().length ? initializeApp({credential: admin.credential.cert(serviceAccount)}) : getApps()[0];
+const appAdmin = !getApps().length
+  ? initializeApp({ credential: admin.credential.cert(JSON.parse(serviceAccount)) })
+  : getApps()[0];
 
 // Xuất Firestore admin để sử dụng
 export const db = getFirestore(appAdmin);
