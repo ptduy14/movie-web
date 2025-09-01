45ea4296533b6b7db5d6623f07f363c99184a244
ptduy14 <phantanduy14@gmail.com>
2025-09-01 15:43:34 +0700

    fix(UI): update image handling in ActorItem component and add new image domain

--- components/actor/actor-item.tsx
+++ components/actor/actor-item.tsx
@@ -1,22 +1,41 @@
-import Actor from "types/actor";
-import ActorImgDefault from "../../public/default-actor-img.jpg";
+import Actor from 'types/actor';
+import ActorImgDefault from '../../public/default-actor-img.jpg';
+import Image from 'next/image';
 
 export default function ActorItem({ actor }: { actor: Actor | string }) {
   // Kiểm tra nếu actor là chuỗi (chỉ tên diễn viên)
   if (typeof actor === 'string') {
     return (
       <div className="w-full text-center">
-        <div className="w-full h-36 rounded-full overflow-hidden bg-cover" style={{backgroundImage: `url(${ActorImgDefault.src})`}}>
+        <div className="w-full aspect-square rounded-full overflow-hidden relative">
+          <Image
+            src={ActorImgDefault}
+            alt={actor}
+            fill
+            className="object-cover object-center"
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          />
         </div>
         <div className="mt-3">{actor}</div>
       </div>
     );
-  } 
+  }
 
   // Trường hợp actor là đối tượng đầy đủ
   return (
     <div className="w-full text-center">
-      <div className="w-full aspect-square rounded-full overflow-hidden bg-cover bg-no-repeat" style={{backgroundImage: actor.profile_path ? `url(https://image.tmdb.org/t/p/w300${actor.profile_path})` : `url(${ActorImgDefault.src})`}}>
+      <div className="w-full aspect-square rounded-full overflow-hidden relative">
+        <Image
+          src={
+            actor.profile_path
+              ? `${process.env.NEXT_PUBLIC_TMDB_IMG_DOMAIN}/t/p/w300${actor.profile_path}`
+              : ActorImgDefault
+          }
+          alt={actor.name}
+          fill
+          className="object-cover object-center"
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        />
       </div>
       <div className="mt-3">{actor.name}</div>
       <div className="text-sm text-gray-400">{actor.character}</div>
